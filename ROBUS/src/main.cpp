b9e92ec886fe5c61ca5608edd5b8b0c7f3c767d2
Alexandre Bernier <bera1107@usherbrooke.ca>
2020-11-09 19:26:01 -0500

    PID satisfaisant

--- ROBUS/src/main.cpp
+++ ROBUS/src/main.cpp
@@ -6,44 +6,49 @@
 
 /******************************************************************************/
 /* main --------------------------------------------------------------------- */
-void setup() {
-  BoardInit();
-  
-  // Initialisations
-  Deplacement_Init();
+void setup()
+{
+    BoardInit();
 
-  print("\n Début de programme : %d --------------------------------- \n", millis());
+    // Initialisations
+    Deplacement_Init();
 
-  // Conditions initiales
-  Deplacement_Ligne(100);
+    print("\n Début de programme : %d --------------------------------- \n", millis());
+
+    // Conditions initiales
+    Deplacement_Ligne(100);
 }
 
-void loop() {
-  extern float commandeG;
-  extern float commandeD;
-  extern float commandeVitesse;
+void loop()
+{
+    extern float commandeG;
+    extern float commandeD;
+    extern float commandeVitesse;
 
-  extern float vitesseG;
-  extern float vitesseD;
-  extern int32_t valEncodeurG;
-  extern int32_t valEncodeurD;
+    extern float   vitesseG;
+    extern float   vitesseD;
+    extern int32_t valEncodeurG;
+    extern int32_t valEncodeurD;
 
-  extern float multiplicateurG;
-  extern float multiplicateurD;
+    extern float multiplicateurG;
+    extern float multiplicateurD;
 
-  if (Deplacement_Fini() == false)
-  {
-    print("Commande: %ld \t %ld\n", (int32_t)commandeG, (int32_t)(commandeVitesse * 1000));
-    print("Vitesse: %ld, %ld\n", (int32_t)(vitesseG * 1000), (int32_t)(vitesseD * 1000));
-    print("mult: %ld, %ld\n", (int32_t)(multiplicateurG * 1000), (int32_t)(multiplicateurD * 1000));
-    print("Encodeur: %ld, %ld\n", valEncodeurG, valEncodeurD);
-    print("-----\n");
-    delay(50);
-  }
-  else
-  {
-    print("\nDéplacement terminé à %d, cmdG: %ld, cmdD: %ld\n", millis(), commandeG, commandeD);
-    while(true){}
-  }
-  
+    if(Deplacement_Fini() == false)
+    {
+        print("Commande: %ld \t %ld\n", (int32_t)commandeG, (int32_t)(commandeVitesse * 1000));
+        print("Vitesse: %ld, %ld\n", (int32_t)(vitesseG * 1000), (int32_t)(vitesseD * 1000));
+        print(
+          "mult: %ld, %ld\n", (int32_t)(multiplicateurG * 1000), (int32_t)(multiplicateurD * 1000));
+        print(
+          "Encodeur: %ld, %ld, %ld\n", valEncodeurG, valEncodeurD, (valEncodeurG - valEncodeurD));
+        print("-----\n");
+        delay(50);
+    }
+    else
+    {
+        print("\nDéplacement terminé à %d, cmdG: %ld, cmdD: %ld\n", millis(), commandeG, commandeD);
+        while(true)
+        {
+        }
+    }
 }
